614ae9753deb40e26859b036db4f70cf07b59d71
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-01 18:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var isDate = function (input) {
   //   write your code here
-};
+	if(input instanceof Date)return true;
+	else if(Date.parse(input)){
+		return true;
+	}
+	else return false; 
+};  
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
